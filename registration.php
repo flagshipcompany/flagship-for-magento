549c6a07d5c35ae744247b9624674e16d197fedb
radua <radua@outlook.com>
2019-06-21 14:38:48 +0000

    Updates version number

--- registration.php
+++ registration.php
@@ -13,4 +13,4 @@ require BP.'/vendor/autoload.php';
 define('SMARTSHIP_WEB_URL','https://smartship-ng.flagshipcompany.com');
 define('SMARTSHIP_API_URL','https://api.smartship.io');
 define('FLAGSHIP_MODULE','Magento');
-define('FLAGSHIP_MODULE_VERSION','1.0.16');
+define('FLAGSHIP_MODULE_VERSION','1.0.17');
