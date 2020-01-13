9105fc1c710773362f8983e78f3f22abccc68c5a
radua <radua@outlook.com>
2020-01-13 18:21:18 +0000

    Update version

--- registration.php
+++ registration.php
@@ -10,4 +10,4 @@ ComponentRegistrar::register(
 require BP.'/vendor/autoload.php';
 
 define('FLAGSHIP_MODULE','Magento');
-define('FLAGSHIP_MODULE_VERSION','1.0.31');
+define('FLAGSHIP_MODULE_VERSION','1.0.32');
