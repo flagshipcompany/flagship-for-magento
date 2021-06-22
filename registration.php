dd42f30827fd490e6814ea3270124b2d2a0836f5
RicardoRomero <ricardo@flagshipcompany.com>
2021-06-22 08:08:41 -0400

    Updates version number to 1.0.54

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 
-use \Magento\Framework\Component\ComponentRegistrar;
+use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
@@ -10,4 +10,4 @@ ComponentRegistrar::register(
 require BP.'/vendor/autoload.php';
 
 define('FLAGSHIP_MODULE', 'Magento');
-define('FLAGSHIP_MODULE_VERSION', '1.0.53');
+define('FLAGSHIP_MODULE_VERSION', '1.0.54');
